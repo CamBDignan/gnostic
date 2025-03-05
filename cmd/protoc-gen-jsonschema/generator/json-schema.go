d9543a30873668467f4d4d27f952ebf502c84a9b
Cameron Dignan <cdignan@esri.com>
2025-03-05 02:29:33 -0500

    cleanup

--- cmd/protoc-gen-jsonschema/generator/json-schema.go
+++ cmd/protoc-gen-jsonschema/generator/json-schema.go
@@ -260,7 +260,7 @@ func (g *JSONSchemaGenerator) buildSchemasFromMessages(messages []*protogen.Mess
 
 	// For each message, generate a schema.
 	for _, message := range messages {
-		schemaName := string(message.Desc.Name())
+		schemaName := messageDefinitionName(message.Desc)
 		typ := "object"
 		id := fmt.Sprintf("%s%s.json", *g.conf.BaseURL, schemaName)
 
@@ -284,14 +284,6 @@ func (g *JSONSchemaGenerator) buildSchemasFromMessages(messages []*protogen.Mess
 		if message.Messages != nil {
 			for _, subMessage := range message.Messages {
 				subSchemas := g.buildSchemasFromMessages([]*protogen.Message{subMessage})
-				if len(subSchemas) != 1 {
-					continue
-				}
-				subSchema := subSchemas[0]
-				subSchema.Value.ID = nil
-				subSchema.Value.Schema = nil
-				subSchema.Name = messageDefinitionName(subMessage.Desc)
-
 				schemas = append(schemas, subSchemas...)
 			}
 		}
